6e3209e93856c8b31aa1f176ec7a35f9bf557a13
Gianmarco Pettinato <gianmarco@pettinato.eu>
2022-02-05 11:13:15 +0100

    Update sway config

--- .config/sway/scripts/startup.sh
+++ .config/sway/scripts/startup.sh
@@ -9,4 +9,4 @@ export XDG_CURRENT_DESKTOP=Unity
 /home/gianm/.config/waybar/launch.sh &
 oguri-swaybg &
 sleep 1s
-~/.config/sway/scripts/wallpaper.sh ~/Immagini/wallpaper/NordWallpaper 600 &
+~/.config/sway/scripts/wallpaper.sh ~/Pictures/wallpaper/NordWallpaper 600 &
